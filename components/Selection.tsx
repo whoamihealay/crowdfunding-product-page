97bd58c049e1a3c98d4b6149dd754bc3bac72256
Andrew SOUTHERN <whoamihealay@gmail.com>
2021-12-18 10:48:41 -0500

    Added dark background when selection and thanks component are active

--- components/Selection.tsx
+++ components/Selection.tsx
@@ -14,25 +14,27 @@ const Selection = ({ data, children, onClick }: SelectionProps) => {
   const hook = data.header.hook;
 
   return (
-    <section
-      id="selection"
-      className="px-4 mx-auto fixed overflow-y-scroll overflow-x-hidden top-0 left-0 right-0 bottom-0 bg-white rounded-lg shadow-2xl shadow-black w-full p-4 mt-32 mb-4 max-w-[90%] sm:max-w-3xl"
-    >
-      <header className="flex justify-between items-center p-4">
-        <h2 className="text-lg text-black font-bold">{title}</h2>
-        <div className="cursor-pointer">
-          <Image
-            src="/images/icon-close-modal.svg"
-            alt="close"
-            width={30}
-            height={30}
-            onClick={onClick}
-          />
-        </div>
-      </header>
-      <p className="pb-4 px-4">{hook}</p>
-      <div className="flex flex-col gap-4">{children}</div>
-    </section>
+    <div className="before:bg-gray-800/50 before:absolute before:inset-x-0 before:top-0 before:-bottom-[200%]">
+      <section
+        id="selection"
+        className="px-4 mx-auto fixed overflow-y-scroll overflow-x-hidden top-0 left-0 right-0 bottom-0 bg-white rounded-lg  w-full p-4 mt-32 mb-4 max-w-[90%] sm:max-w-3xl"
+      >
+        <header className="flex justify-between items-center p-4">
+          <h2 className="text-lg text-black font-bold">{title}</h2>
+          <div className="cursor-pointer">
+            <Image
+              src="/images/icon-close-modal.svg"
+              alt="close"
+              width={30}
+              height={30}
+              onClick={onClick}
+            />
+          </div>
+        </header>
+        <p className="pb-4 px-4">{hook}</p>
+        <div className="flex flex-col gap-4">{children}</div>
+      </section>
+    </div>
   );
 };
 
--- components/Thanks.tsx
+++ components/Thanks.tsx
@@ -13,21 +13,23 @@ const Thanks = ({ data, onClick }: ThanksProps) => {
   const check = data.images.check;
 
   return (
-    <article className="px-4 m-auto text-center fixed overflow-y-scroll overflow-x-hidden inset-0 bg-white rounded-lg shadow-2xl shadow-black w-full p-4 max-w-[90%] sm:max-w-lg sm:max-h-96">
-      {check ? (
-        <div className="flex justify-center p-4">
-          <Image src={check} alt="check" width={64} height={64} />
-        </div>
-      ) : null}
-      <h1 className="text-2xl font-bold text-black p-4">{title}</h1>
-      <p className="text-base px-4 pb-4">{description}</p>
-      <button
-        onClick={onClick}
-        className="bg-cyan-bg rounded-[2rem] m-4 py-4 px-8 text-white font-bold cursor-pointer hover:bg-cyan-active"
-      >
-        {cta}
-      </button>
-    </article>
+    <div className="before:bg-gray-800/50 before:absolute before:inset-x-0 before:top-0 before:-bottom-[200%]">
+      <article className="px-4 m-auto text-center fixed overflow-y-scroll overflow-x-hidden inset-0 bg-white rounded-lg shadow-2xl shadow-black w-full p-4 max-w-[90%] sm:max-w-lg sm:max-h-96">
+        {check ? (
+          <div className="flex justify-center p-4">
+            <Image src={check} alt="check" width={64} height={64} />
+          </div>
+        ) : null}
+        <h1 className="text-2xl font-bold text-black p-4">{title}</h1>
+        <p className="text-base px-4 pb-4">{description}</p>
+        <button
+          onClick={onClick}
+          className="bg-cyan-bg rounded-[2rem] m-4 py-4 px-8 text-white font-bold cursor-pointer hover:bg-cyan-active"
+        >
+          {cta}
+        </button>
+      </article>
+    </div>
   );
 };
 
